21a57dfc5052cf3a736c7333ee4d6d23aa190940
Natan Pezzini <natanlenhard@hotmail.com>
2022-09-29 19:03:55 -0300

    Codigo limpo

---
+++ cod_limpo.py
@@ -0,0 +1,10 @@
+
+
+x = str(input("escreava um valor:"))
+    
+match x:
+    case '0': print("zero") 
+    case '1': print("um") 
+    case '2': print("dois")   
+    case _: print(":p")      
+   
---
+++ cod_nao_limpo.py
@@ -0,0 +1,10 @@
+
+x = int(input("escreava um valor:"))
+if x == 0:
+    print("zero")
+elif x == 1:
+    print("um")
+elif x == 2:
+    print("Dois")
+else:
+   print(":p")
